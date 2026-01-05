1c3afbcac9c95d9bef9687a957fef46eaef771bf
mohammad ali baymani <buymoney.10@gmail.com>
2026-01-05 23:27:18 +0330

    fix bug 4

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx یا app/_not-found.tsx
+export default function NotFound() {
+    return (
+      <div>
+        <h2>Page Not Found</h2>
+        <p>Could not find requested resource</p>
+      </div>
+    );
+  }
