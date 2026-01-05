dfe829cc3394eac531574e7f15981da7b2d0f8d7
mohammad ali baymani <buymoney.10@gmail.com>
2026-01-05 22:43:54 +0330

    add vercel json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // یا 'export' برای static export
+  trailingSlash: true, // برای Vercel بهتر است
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // اگر مشکل image دارید
+  },
 };
 
 export default nextConfig;
